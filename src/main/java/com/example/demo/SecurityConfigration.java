20ae0088decf6280a46ca85bf18170b690a39407
petivip <petivip@gmail.com>
2019-12-03 10:41:02 -0500

    another change

--- src/main/java/com/example/demo/SecurityConfigration.java
+++ src/main/java/com/example/demo/SecurityConfigration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -17,10 +18,10 @@ public class SecurityConfigration extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
+        http.authorizeRequests().antMatchers("/").access("hasAnyAuthority('USER','ADMIN')").antMatchers("/admin").access("hasAuthority('ADMIN')").anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll();
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication().withUser("users").password(passwordEncoder().encode("pass")).authorities("USER");
+        auth.inMemoryAuthentication().withUser("petros").password(passwordEncoder().encode("petros")).authorities("ADMIN").and().withUser("users").password(passwordEncoder().encode("pass")).authorities("USER");
     }
 }
